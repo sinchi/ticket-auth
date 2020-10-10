71ce648bdafc16416377448f2fe4c91d838f82a6
ayoub <ayoub.belghar@gmail.com>
2020-10-10 03:04:46 +0200

    add tests

---
+++ src/routes/user/__test__/signup.test.ts
@@ -0,0 +1,64 @@
+import request from 'supertest'
+import { app } from '../../../app'
+
+it('retunr 201 on succesfull request', async () => {
+  return request(app)
+        .post('/api/users/signup')
+        .send({
+          email: 'test@test.com',
+          password: '12345'
+        })
+        .expect(201);
+});
+
+it('retunr 400 with invalid email', async () => {
+  return request(app)
+        .post('/api/users/signup')
+        .send({
+          email: 'test@test',
+          password: '12345'
+        })
+        .expect(400);
+})
+
+it('retunr 400 with invalid password', async () => {
+  return request(app)
+        .post('/api/users/signup')
+        .send({
+          email: 'test@test',
+          password: '12'
+        })
+        .expect(400);
+})
+
+it('retunr 400 with missing email and password', async () => {
+  await request(app)
+        .post('/api/users/signup')
+        .send({    
+          email: 'ayoub@sinhci.com'     
+        })
+        .expect(400);
+  await request(app)
+        .post('/api/users/signup')
+        .send({  
+          password: '123456'       
+        })
+        .expect(400);
+})
+
+it('disallows duplicate emails', async () => {
+  await request(app)
+        .post('/api/users/signup')
+        .send({
+          email: 'test@test.com',
+          password: '12334'
+        })
+        .expect(201);
+  await request(app)
+        .post('/api/users/signup')
+        .send({
+          email: 'test@test.com',
+          password: '12334'
+        })
+        .expect(400);
+})

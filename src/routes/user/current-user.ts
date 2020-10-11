7b1dbc4f613f1df87098d24ac4442f1a9ea142e9
ayoub <ayoub.belghar@gmail.com>
2020-10-11 15:45:15 +0200

    add current-user handler

--- src/routes/user/current-user.ts
+++ src/routes/user/current-user.ts
@@ -1,9 +1,22 @@
-import express from 'express';
-
+import express, { response } from 'express';
+import jwt from 'jsonwebtoken'; 
+import { User } from '../../models/user';
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('current user')
+
+  // read token from request body
+  if(!req.session?.jwt) {
+    return res.status(401).send({ currentUser: null });
+  }
+  
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    return res.status(200).send({ currentUser: payload });
+  }catch(error) {
+    return res.status(400).send({ currentUser: null });
+  }  
+
 });
 
 export { router as currentUserRouter };
